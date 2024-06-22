3402b121da8302597fe5c6363cf0016f1540d5fe
Prassu <Prassu@gmail.com>
2024-06-22 12:20:15 +0530

    all committed

---
+++ src/main/java/com/example/pipeline_exmple/PipelineExmpleApplication.java
@@ -0,0 +1,13 @@
+package com.example.pipeline_exmple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineExmpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineExmpleApplication.class, args);
+	}
+
+}
